ccad13f28d70d055a2fa8a0c96115f942848403f
subhajit007 <subhajitusa007@gmail.com>
2015-06-24 16:15:59 -0700

    first commit

---
+++ app/models/weather_lookup.rb
@@ -0,0 +1,16 @@
+class WeatherLookup < ActiveRecord::Base
+
+        def fetch_weather
+        HTTParty.get("http://api.wunderground.com/api/API_KEY/hourly/q/20001.xml")
+end
+    attr_accessor :temperature, :icon
+         def initialize
+          weather_hash = fetch_weather
+          assign_values(weather_hash)
+end
+          def assign_values(weather_hash)
+          hourly_forecast_response = weather_hash.parsed_response['response']['hourly_forecast']['forecast'].first
+          self.temperature = hourly_forecast_response['temp']['english']
+          self.icon = hourly_forecast_response['icon_url']
+end
+end
